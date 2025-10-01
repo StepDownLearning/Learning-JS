fc928add05f4885ae26fb5633586cd9cae355df0
Jacob <104500838+JustExisting1@users.noreply.github.com>
2025-10-01 22:12:26 +0100

    Demo Update

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-//I made a chgange
+/** @format */
+
+let firstName = "Jacob"; //Variable that holds data, value has been set;
+const age = 13; //Variable that is constant, the value cannot be changed
+let trueBool = true; //Boolean variable, true or false.
+
+const nameInput = document.getElementById("name"); //Stored the element with id="name" into this variable
+const ageInput = document.getElementById("age"); // This lets us access the value of this element in our function below
+const outputElement = document.getElementById("output");
+
+///function functionName (parameters){}
+/** @summary
+ * Linked to a button. When the button is pressed this function will run.
+ * @description Changes the value of an element
+ * @returns null
+ */
+function printNameAge() {
+  //Changes the text of the outputElement = values from input fields...
+  outputElement.textContent = `Name: ${nameInput.value}. Age: ${ageInput.value}`;
+}
