a1156280a2f243053197ae4b15f63443d9fc183b
Mitchy <MitchoppDEV@gmail.com>
2025-10-01 20:43:44 +0100

    absolute peak if statement

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ console.log("Hello world");
 
 let firstName = "Mitch";
 console.log(firstName);
+
+const age = 19
+const number = 50
+
+console.log(age + number);
+if (age >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Child");
+}
