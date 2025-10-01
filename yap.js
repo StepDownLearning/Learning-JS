93fd4e83281f6029c1ec9566d41e0f9fc87b901d
Mitchy <MitchoppDEV@gmail.com>
2025-10-01 21:13:59 +0100

    javascript

---
+++ yap.js
@@ -0,0 +1,6 @@
+const age = 19;
+const year = 2025;
+console.log("Age:", age, "Year:", year);
+
+const nameInput = document.getElementById("name");
+const ageInput = document.getElementById("year");
